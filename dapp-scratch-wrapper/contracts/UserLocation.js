c5bd9e6e98145812175f895f103ae58093d4139e
Till Kolter <till.kolter@gmail.com>
2017-12-07 04:07:28 +0100

    Update contracts

--- dapp-scratch-wrapper/contracts/UserLocation.js
+++ dapp-scratch-wrapper/contracts/UserLocation.js
@@ -10,7 +10,7 @@ class UserLocation {
   constructor (contractManager, options) {
 
     this.contractManager = contractManager
-    this.address = '0x345ca3e014aaf5dca488057592ee47305d9b3e10'
+    this.address = '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf'
     this.genesisBlock = 0
     this.options = {
       getPastEvents: false,
